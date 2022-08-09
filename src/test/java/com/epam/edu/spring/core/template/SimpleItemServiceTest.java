bb03e871262925ab4871faff596677000043f5d5
Dmitrii Khaimin <khaimindm@gmail.com>
2022-08-09 12:33:22 +0400

    09.08.2022-2

--- src/test/java/com/epam/edu/spring/core/template/SimpleItemServiceTest.java
+++ src/test/java/com/epam/edu/spring/core/template/SimpleItemServiceTest.java
@@ -1,5 +1,7 @@
 package com.epam.edu.spring.core.template;
 
+import com.epam.edu.spring.core.template.entity.Color;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +18,13 @@ public class SimpleItemServiceTest {
     @Autowired
     private Item item;
 
+    private Color color;
+
     @Test
     public void checkCreateItem() {
-        item = new Item(10, test, 10, color);
-        Assert.isTrue(item.getPrice() == 10, "The Item price is not 10");
+        item = new Item(10, "test", 10, color);
+        Assert.assertTrue(item.getPrice() == 10);
+        //Assert.isTrue(item.getPrice() == 10, "The Item price is not 10");
     }
 
 }
